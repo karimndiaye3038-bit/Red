b18c0ace6e296ff8fa808825f67234c3b83ba6f8
unknown <karindiaye3038@gmail.com>
2026-04-17 21:17:59 +0000

    Voici la partie frotend du projet Red Product

---
+++ Frontend/Javascript/script.js
@@ -0,0 +1,36 @@
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+// fermer si on clique en dehors
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden");
+  }
+});
+
+//  pour que le bouton ferme
+  const menufot = document.getElementById("menufot");
+  const sidebare = document.getElementById("sidebare");
+
+  menufot.addEventListener("click", () => {
+    sidebare.classList.toggle("-translate-x-full");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!sidebare.contains(e.target) && !menufot.contains(e.target)) {
+      sidebare.classList.add("-translate-x-full");
+    }
+  });
+  
+
+  
